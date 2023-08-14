0d4b2b4092c6aeeb72e03bc4cb0f94a183be63e6
yalgar0 <dk.yalgar@gmail.com>
2023-08-14 15:21:32 +0500

    age-calculator

---
+++ index.js
@@ -0,0 +1,36 @@
+const brithdayE1 = document.getElementById("birthday");
+const btnE1 = document.getElementById("btn");
+const resultE1 = document.getElementById("result");
+
+
+
+function calculateAge() {
+    const birthdayValue = brithdayE1.value;
+    if (birthdayValue == "") {
+        alert("Please Enter your birthday");
+    }
+    else {
+        const age = getAge(birthdayValue);
+        resultE1.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+    }
+}
+
+
+
+function getAge(birthdayValue) {
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthdayValue);
+    const age = currentDate.getFullYear() - birthdayDate.getFullYear();
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+    if (
+        month < 0 ||
+        (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+      ) {
+        age--;
+      }
+      return age;
+}
+
+
+btnE1.addEventListener("click",calculateAge);
